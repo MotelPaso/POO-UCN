2e6eaea9541e084b198e00e37ffd2b1271422ff4
Paulo Araya <paulo.araya02@alumnos.ucn.cl>
2025-12-09 17:10:28 -0300

    quitar consola y mover a GUI

--- src/dominio/App.java
+++ src/dominio/App.java
@@ -13,9 +13,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
-	
+
 	private static GUI ventana = new GUI();
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		Sistema sistema = SistemaImpl.getInstancia();
 		Scanner s = new Scanner(System.in);
@@ -24,29 +24,8 @@ public class App {
 	}
 
 	private static void iniciarSesion(Scanner s, Sistema sistema) {
-		boolean logged = false;
-		do {
-			System.out.println("===================");
-			System.out.print("Ingrese su correo o nombre de usuario: ");
-			String correo = s.nextLine();
-			System.out.print("Ingrese su contraseña: ");
-			String password = s.nextLine();
-			logged = sistema.revisarUsuario(new String[] {correo, password});
-			if (!logged) {
-				System.out.println("Credenciales incorrectas...");
-			} else {
-				int acceso = sistema.getNivelAcceso(new String[] {correo, password});
-				switch (acceso) {
-				case 1 -> ventana.menuAdmin();
-				case 2 -> ventana.menuColab();
-				case 3 -> ventana.menuEstudiante();
-				case 0 -> System.out.println("Ha habido un error...");
-				}
-			}
-		} while (!logged);
+		ventana.inicioSesion();
 	}
-
-	
 
 	private static void cargarArchivos(Sistema sistema) throws FileNotFoundException {
 		Scanner s = new Scanner(new File("usuarios.txt"));
--- src/presentacion/GUI.java
+++ src/presentacion/GUI.java
@@ -1,19 +1,98 @@
 package presentacion;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 
-public class GUI extends JFrame{
-	
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import dominio.Sistema;
+import dominio.SistemaImpl;
+
+public class GUI extends JFrame {
+
+	private Sistema sistema = SistemaImpl.getInstancia();
+
+	public GUI() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 400);
+
+	}
+
+	public void inicioSesion() {
+		JPanel inicio = new JPanel();
+		inicio.setLayout(new BoxLayout(inicio, BoxLayout.PAGE_AXIS));
+		inicio.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		JLabel userLabel = new JLabel("Ingrese aqui su correo / nombre de usuario");
+		JTextField username = new JTextField(20);
+		JLabel passLabel = new JLabel("Ingrese aqui su contraseña");
+		JTextField password = new JTextField(30);
+		JButton login = new JButton("Ingresar");
+
+		userLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		username.setAlignmentX(Component.CENTER_ALIGNMENT);
+		passLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		password.setAlignmentX(Component.CENTER_ALIGNMENT);
+		login.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		username.setMaximumSize(new Dimension(300, 50));
+		password.setMaximumSize(new Dimension(300, 50));
+
+		login.addActionListener((e) -> {
+			boolean logged = sistema.revisarUsuario(new String[] { username.getText(), password.getText() });
+			if (logged) {
+				int acceso = sistema.getNivelAcceso(new String[] { username.getText(), password.getText() });
+				getContentPane().removeAll();
+				switch (acceso) {
+				case 1 -> menuAdmin();
+				case 2 -> menuColab();
+				case 3 -> menuEstudiante();
+				case 0 -> System.out.println("Ha habido un error...");
+				}
+				revalidate();
+				repaint();
+			} else {
+				username.setText("");
+				password.setText("");
+			}
+		});
+
+		inicio.add(userLabel);
+		inicio.add(username);
+		inicio.add(passLabel);
+		inicio.add(password);
+		inicio.add(login);
+
+		getContentPane().add(inicio);
+		setVisible(true);
+	}
 
 	public void menuEstudiante() {
-		// TODO Auto-generated method stub
+		JLabel titulo = new JLabel("¡Bienvenido!");
+		JButton logout = new JButton("Cerrar Sesión");
+		JPanel menuEstudiante = new JPanel(new BorderLayout());
+
+		menuEstudiante.add(titulo);
+		menuEstudiante.add(logout);
 	}
 
 	public void menuColab() {
-		// TODO Auto-generated method stub
+
 	}
 
 	public void menuAdmin() {
-		// TODO Auto-generated method stub
+		JLabel admin = new JLabel("¡Bienvenido Administrador!");
+		JButton logout = new JButton("Cerrar Sesión");
+		JPanel menuAdmin = new JPanel(new BorderLayout());
+		menuAdmin.add(admin, BorderLayout.CENTER);
+		menuAdmin.add(logout, BorderLayout.SOUTH);
+
+		getContentPane().add(menuAdmin);
 	}
 }
